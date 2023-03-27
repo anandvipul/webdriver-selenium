f63c913ad90a0b8ae718abf8ebe9b95c43212fe4
anandvipul <anandvipul01@gmail.com>
2023-03-27 09:47:17 +0530

    Finding Elements

--- library.js
+++ library.js
@@ -1,5 +1,15 @@
-let webdriver = require("selenium-webdriver");
+let webdriver = require("selenium-webdriver"),
+  By = webdriver.By,
+  until = webdriver.until;
 
 let driver = new webdriver.Builder().forBrowser("chrome").build();
 
-driver.get("https://google.com");
+let url = "https://library-app.firebaseapp.com";
+driver.get(url);
+driver.findElement(By.css("input"));
+// driver.findElement(By.css(".btn-lg"));
+driver.findElements(By.css("nav li")).then((arr) => {
+  console.log("Found Array of Elements: ", arr);
+});
+// driver.sleep(10000);
+// driver.quit();
