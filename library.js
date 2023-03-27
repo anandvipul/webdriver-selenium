00918e4e99e8e89f8ff7c634a43b9f68984e7c80
anandvipul <anandvipul01@gmail.com>
2023-03-27 11:26:39 +0530

    gettext()

--- library.js
+++ library.js
@@ -2,14 +2,18 @@ let webdriver = require("selenium-webdriver"),
   By = webdriver.By,
   until = webdriver.until;
 
-let driver = new webdriver.Builder().forBrowser("chrome").build();
-
 let url = "https://library-app.firebaseapp.com";
-driver.get(url);
-driver.findElement(By.css("input"));
-// driver.findElement(By.css(".btn-lg"));
-driver.findElements(By.css("nav li")).then((arr) => {
-  console.log("Found Array of Elements: ", arr);
-});
-// driver.sleep(10000);
-// driver.quit();
+
+let driver = new webdriver.Builder().forBrowser("chrome").build();
+driver
+  .manage()
+  .window()
+  .setRect({ width: 1024, height: 600 })
+  .then(() => {
+    driver.get(url);
+    driver.findElements(By.css(".nav-link")).then((arr) => {
+      Array.from(arr).map((item) =>
+        item.getText().then((data) => console.log(data))
+      );
+    });
+  });
